9ba441ba3e5d9b4602241dad12c180a3b3cb0457
yosuzuki <yosuzuki@techmatrix.co.jp>
2024-09-09 08:46:44 +0900

    2nd commit

--- MyApplication/hello.c
+++ MyApplication/hello.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+int add(int a, int b);
 
 int main()
 {
     printf("Hello, world!\n");
-    //change
+}
+
+void func()
+{
+    if (add(100, 100) > 0)
+    {
+        printf("More than 0 \n");
+    }
+    else
+    {
+        printf("Less than equal to 0 \n");
+    }
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
